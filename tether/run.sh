3cf83f30b446e663093d75ae72ea5e3f046ee6ce
Henry Strickland <strick@yak.net>
2026-07-04 00:37:39 -0400

    tether/run.sh

---
+++ tether/run.sh
@@ -0,0 +1,12 @@
+echo "Verbose logging & errors are in '_log'" >&2
+
+# You may replace `go run ./.` with one of the pre-compiled
+# binaries in the build/ directory.
+go run ./. \
+    --centipede  \
+    --disks=f0:build/OPIL_BR_2026-04-22.dsk \
+    --no_modules \
+    --n \
+    --abslists ../misc/diskbasic.0x8000.list  \
+    ../misc/*.rom  \
+    2>_log
