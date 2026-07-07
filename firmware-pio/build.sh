002f2620f3acb84d3aaae229be69b61a7385ee53
Henry Strickland <strick@yak.net>
2026-07-07 15:14:00 -0400

    OKAY: firmware-pio runs slow, but logs writes, reads (drive and not)

---
+++ firmware-pio/build.sh
@@ -0,0 +1,12 @@
+SHELF="$(cd ../.. && echo $PWD)"
+mkdir -p build 
+cd build
+
+export PICO_EXAMPLES_PATH=/dev/null
+export PICO_SDK_PATH=$SHELF/pico-sdk
+export PICOTOOL_FETCH_FROM_GIT_PATH=$SHELF/picotool/
+
+set -ex
+cmake ..
+make
+ls -l *.uf2
