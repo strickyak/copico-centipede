1e0e9a21a5a63c74cf980a3fdc704ef336f1c516
Henry Strickland <strick@yak.net>
2026-07-17 00:03:41 -0400

    mkdir, rmdir, cat

--- firmware-pio/littlefs.h
+++ firmware-pio/littlefs.h
@@ -30,29 +30,74 @@ const struct lfs_config lfs = {
 lfs_t lfs_volume;
 
 script::errstring dir_command(const std::vector<std::string>& argv) {
-    lfs_dir_t dir;
-    int err = lfs_dir_open(&lfs_volume, &dir, "/");
-    if (err) {
-        return "Failed to open root directory";
+    std::vector<std::string> dirs;
+    if (argv.size() < 2) {
+        dirs.push_back(".");
+    } else {
+        for (size_t i = 1; i < argv.size(); i++) {
+            dirs.push_back(argv[i]);
+        }
     }
     
-    struct lfs_info info;
-    while (true) {
-        int res = lfs_dir_read(&lfs_volume, &dir, &info);
-        if (res < 0) {
-            lfs_dir_close(&lfs_volume, &dir);
-            return "Error reading directory";
+    for (const auto& d : dirs) {
+        lfs_dir_t dir;
+        int err = lfs_dir_open(&lfs_volume, &dir, d.c_str());
+        if (err) {
+            return "Failed to open directory: " + d;
         }
-        if (res == 0) {
-            break;
+        
+        if (dirs.size() > 1) {
+            printf("%s:\n", d.c_str());
         }
-        if (strcmp(info.name, ".") == 0 || strcmp(info.name, "..") == 0) {
-            continue;
+        
+        struct lfs_info info;
+        while (true) {
+            int res = lfs_dir_read(&lfs_volume, &dir, &info);
+            if (res < 0) {
+                lfs_dir_close(&lfs_volume, &dir);
+                return "Error reading directory: " + d;
+            }
+            if (res == 0) {
+                break;
+            }
+            if (strcmp(info.name, ".") == 0 || strcmp(info.name, "..") == 0) {
+                continue;
+            }
+            if (info.type == LFS_TYPE_DIR) {
+                printf("%s/\n", info.name);
+            } else {
+                printf("%s\n", info.name);
+            }
         }
-        printf("%s\n", info.name);
+        
+        lfs_dir_close(&lfs_volume, &dir);
     }
-    
-    lfs_dir_close(&lfs_volume, &dir);
+    return "";
+}
+
+script::errstring mkdir_command(const std::vector<std::string>& argv) {
+    if (argv.size() < 2) {
+        return "Usage: mkdir dir...";
+    }
+    for (size_t i = 1; i < argv.size(); i++) {
+        int err = lfs_mkdir(&lfs_volume, argv[i].c_str());
+        if (err < 0) {
+            return "Failed to mkdir: " + argv[i];
+        }
+    }
+    return "";
+}
+
+script::errstring rmdir_command(const std::vector<std::string>& argv) {
+    if (argv.size() < 2) {
+        return "Usage: rmdir dir...";
+    }
+    for (size_t i = 1; i < argv.size(); i++) {
+        int err = lfs_remove(&lfs_volume, argv[i].c_str());
+        if (err < 0) {
+            return "Failed to rmdir: " + argv[i];
+        }
+    }
     return "";
 }
 
@@ -62,6 +107,84 @@ script::errstring echo_command(const std::vector<std::string>& argv) {
         printf("%s", argv[i].c_str());
     }
     printf("\n");
+    return "";
+}
+
+script::errstring echo_create_command(const std::vector<std::string>& argv) {
+    if (argv.size() < 2) {
+        return "Usage: echo-create filename [args...]";
+    }
+    
+    lfs_file_t file;
+    int err = lfs_file_open(&lfs_volume, &file, argv[1].c_str(), LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
+    if (err < 0) {
+        return "Failed to create file: " + argv[1];
+    }
+    
+    for (size_t i = 2; i < argv.size(); i++) {
+        if (i > 2) {
+            lfs_file_write(&lfs_volume, &file, " ", 1);
+        }
+        lfs_file_write(&lfs_volume, &file, argv[i].c_str(), argv[i].length());
+    }
+    lfs_file_write(&lfs_volume, &file, "\n", 1);
+    
+    lfs_file_close(&lfs_volume, &file);
+    return "";
+}
+
+script::errstring cat_command(const std::vector<std::string>& argv) {
+    if (argv.size() < 2) {
+        return "Usage: cat [-n] filename...";
+    }
+    
+    bool print_lines = false;
+    size_t start_idx = 1;
+    
+    if (argv.size() > 1 && argv[1] == "-n") {
+        print_lines = true;
+        start_idx = 2;
+        if (argv.size() < 3) {
+            return "Usage: cat [-n] filename...";
+        }
+    }
+    
+    int line_num = 1;
+    bool at_line_start = true;
+    
+    for (size_t i = start_idx; i < argv.size(); i++) {
+        lfs_file_t file;
+        int err = lfs_file_open(&lfs_volume, &file, argv[i].c_str(), LFS_O_RDONLY);
+        if (err < 0) {
+            return "cat: " + argv[i] + ": No such file or directory";
+        }
+        
+        char buf[64];
+        while (true) {
+            lfs_ssize_t res = lfs_file_read(&lfs_volume, &file, buf, sizeof(buf));
+            if (res < 0) {
+                lfs_file_close(&lfs_volume, &file);
+                return "Error reading file: " + argv[i];
+            }
+            if (res == 0) {
+                break;
+            }
+            for (lfs_ssize_t j = 0; j < res; j++) {
+                char ch = buf[j];
+                if (at_line_start) {
+                    if (print_lines) {
+                        printf("%6d  ", line_num++);
+                    }
+                    at_line_start = false;
+                }
+                printf("%c", ch);
+                if (ch == '\n') {
+                    at_line_start = true;
+                }
+            }
+        }
+        lfs_file_close(&lfs_volume, &file);
+    }
     return "";
 }
 
@@ -79,7 +202,11 @@ void init_lfs() {
     }
 
     script::global_script_commands.push_back({ "dir", dir_command });
+    script::global_script_commands.push_back({ "mkdir", mkdir_command });
+    script::global_script_commands.push_back({ "rmdir", rmdir_command });
     script::global_script_commands.push_back({ "echo", echo_command });
+    script::global_script_commands.push_back({ "echo-create", echo_create_command });
+    script::global_script_commands.push_back({ "cat", cat_command });
 }
 
 #endif // FIRMWARE_PIO_LITTLEFS_H_
