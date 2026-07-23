4462744aa229712bf95a9ed44bc09184c7dc95ba
Henry Strickland <strick@yak.net>
2026-07-23 17:46:46 -0400

    cobs both ways works.  but: Missing WAT in WAIT.

--- v1/tether/tconsole.go
+++ v1/tether/tconsole.go
@@ -825,8 +825,6 @@ func RunSelect(inkey chan byte, fromUSB <-chan byte, channelToPico chan []byte, 
 			case C_COMPRESSED_CYCLES: // 175
 				// func DecompressCycles(compressed []byte) []uint32
 				pack := pkt[1:]
-				log.Printf("C_COMPRESSED_CYCLES: pkt len=%d, pack len=%d, pack=% 02x", len(pkt), len(pack), pack)
-				ResetDecompressCycles()
 				cycles := DecompressCycles(pack)
 				for _, cy := range cycles {
 					direction, addr, data := (cy>>24)&0xFF, (cy>>8)&0xFFFF, cy&0xFF
