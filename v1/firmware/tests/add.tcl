3be1ad15ec655330352e799723b90345b02b18a0
Henry Strickland <strick@yak.net>
2026-08-04 18:56:13 -0400

    delete obsolete skills and tests

--- v1/firmware/tests/add.tcl
+++
@@ -1,3 +0,0 @@
-send "expr 39+3\n"
-expect 42
-expect TCL>
--- v1/firmware/tests/wrong_add_timeout.tcl
+++
@@ -1,8 +0,0 @@
-send "expr 39+3\n"
-
-expect {
-    43      {puts ...OKAY\n ; exp_continue }
-    timeout {puts ...TIMEOUT\n ; exit 13 }
-}
-
-expect TCL>
