ef4342e5140d733689ac8cd536caa87edc9f1440
Henry Strickland <strick@yak.net>
2026-08-27 22:10:11 -0400

    minor formatting

--- v1/tether/build-multi.sh
+++ v1/tether/build-multi.sh
@@ -11,13 +11,13 @@ while read EXE ENV
 do
     echo $ENV go build -x -o build/$EXE ./. | sh -x
 done << END
-tether.linux-amd64.exe GOOS=linux GOARCH=amd64 
-tether.linux-386.exe GOOS=linux GOARCH=386    
-tether.linux-arm-7.exe GOOS=linux GOARCH=arm GOARM=7 
-tether.linux-arm64.exe GOOS=linux GOARCH=arm64      
-tether.win-amd64.exe GOOS=windows GOARCH=amd64     
-tether.win-386.exe GOOS=windows GOARCH=386       
-tether.mac-arm64.exe GOOS=darwin GOARCH=arm64   
-tether.mac-amd64.exe GOOS=darwin GOARCH=amd64  
+tether.linux-amd64.exe   GOOS=linux   GOARCH=amd64 
+tether.linux-386.exe     GOOS=linux   GOARCH=386    
+tether.linux-arm-7.exe   GOOS=linux   GOARCH=arm GOARM=7 
+tether.linux-arm64.exe   GOOS=linux   GOARCH=arm64      
+tether.win-amd64.exe     GOOS=windows GOARCH=amd64     
+tether.win-386.exe       GOOS=windows GOARCH=386       
+tether.mac-arm64.exe     GOOS=darwin   GOARCH=arm64   
+tether.mac-amd64.exe     GOOS=darwin   GOARCH=amd64  
 END
 ls -l build/*.exe build/tether
